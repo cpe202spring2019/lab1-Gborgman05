48a9403d9ff59b037e591d743a73a16c302e2f08
Gborgman05 <gborgman@calpoly.edu>
2019-04-09 21:08:53 -0700

    I think this is all I had to do?

--- location_tests.py
+++ location_tests.py
@@ -6,7 +6,13 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        self.assertNotEqual(repr(Location("Narnia", 1, 2)), repr(loc))
+        self.assertEqual(repr(Location("Candyland", 0, 4)), repr(Location("Candyland", 0, 4)))
+    def test_eq(self):
+        loc = Location("SLO", 35.3, -120.7)
+        self.assertEqual(Location("SLO", 35.3, -120.7), Location("SLO", 35.3, -120.7))
+        self.assertEqual(loc, Location("SLO", 35.3, -120.7))
+        self.assertNotEqual(Location("Candyland", 0, 4), Location("America", 0, 4))
     # Add more tests!
 
 if __name__ == "__main__":
